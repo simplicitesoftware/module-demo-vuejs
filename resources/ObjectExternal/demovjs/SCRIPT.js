e3f1aa2e1e25af475aa663b25bec827bf746f9ee
designer (Designer) <no-reply@simplicite.fr>
2019-08-29 23:40:14 +0200

    Created

---
+++ resources/ObjectExternal/demovjs/SCRIPT.js
@@ -0,0 +1,24 @@
+var demovjs = typeof VJSDemo !== 'undefined' ? VJSDemo : (function($) {
+	var app, prd, vue, data = { products: [] };
+	
+	/**
+	 * Render
+	 * @param root Context path
+	 * @param banner Banner URL
+	 * @param pub Public?
+	 * @function
+	 */
+	function render(root, banner, pub) {
+		data.bannerURL = banner; // Image banner URL
+		data.toFixed = function() { return function(n, r) { return parseFloat(r(n)).toFixed(2); } }; // Rendering for decimal
+		if (!pub) $("#demovjs").css("min-height", "1000px");
+		vue = new Vue({ el: '#demovjs', data: data });
+		app = pub ? new Simplicite.Ajax(root, 'api', 'website', 'simplicite') : new Simplicite.Ajax();
+		prd = app.getBusinessObject('DemoProduct');
+		prd.search(function(rows) {
+			data.products = rows;
+		}, null, { inlineDocs: true });
+	}
+
+	return { render: render };	
+})(jQuery);
---
+++ src/com/simplicite/extobjects/DemoVueJS/demovjs.java
@@ -0,0 +1,44 @@
+package com.simplicite.extobjects.VueJSDemo;
+
+import com.simplicite.util.AppLog;
+import com.simplicite.util.ExternalObject;
+import com.simplicite.util.tools.HTMLTool;
+import com.simplicite.util.tools.Parameters;
+import com.simplicite.webapp.web.BootstrapWebPage;
+
+/**
+ * Vue.js demo
+ */
+public class demovjs extends ExternalObject {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Display method
+	 * @param params Request parameters
+	 */
+	@Override
+	public Object display(Parameters params) {
+		try {
+			boolean pub = isPublic();
+			setDecoration(!pub);
+			String render = getName() + ".render('" + HTMLTool.getRoot() + "', '" + HTMLTool.getResourceImageURL(this, "BANNER") + "', " + pub + ");";
+			if (pub) { // Public page version (standalone Bootstrap page)
+				BootstrapWebPage wp = new BootstrapWebPage(params.getRoot(), getDisplay(), false);
+				wp.setFavicon(HTMLTool.getResourceIconURL(this, "FAVICON"));
+				wp.appendAjax(true);
+				wp.appendJSInclude(HTMLTool.getResourceJSURL(getGrant(), "VUEJS"));
+				wp.appendJSInclude(HTMLTool.getResourceJSURL(this, "SCRIPT"));
+				wp.appendCSSInclude(HTMLTool.getResourceCSSURL(this, "STYLES"));
+				wp.append(HTMLTool.getResourceHTMLContent(this, "HTML"));
+				wp.setReady(render);
+				return wp.toString();
+			} else { // Private page version
+				appendJSInclude(HTMLTool.getResourceJSURL(getGrant(), "VUEJS"));
+				return javascript(render);
+			}
+		} catch (Exception e) {
+			AppLog.error(getClass(), "display", null, e, getGrant());
+			return e.getMessage();
+		}
+	}
+}
