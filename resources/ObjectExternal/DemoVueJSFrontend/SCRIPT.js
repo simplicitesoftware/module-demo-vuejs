0b907cd6932ae26edf64d5c508b37c9ebd206e99
designer (Designer) <no-reply@simplicite.fr>
2020-07-08 16:22:18 +0200

    Simplified JS

--- resources/ObjectExternal/DemoVueJSFrontend/SCRIPT.js
+++ resources/ObjectExternal/DemoVueJSFrontend/SCRIPT.js
@@ -1,4 +1,4 @@
-var DemoVueJSFrontend = typeof DemoVueJSFrontend !== 'undefined' ? DemoVueJSFrontend : (function($) {
+var DemoVueJSFrontend = DemoVueJSFrontend || (function($) {
 	var app, prd, data = { list: null, item: null };
 
 	/**
