ed4536184621c33d0b79ab739e5badcd7b08fce8
designer (Designer) <simplicite@simplicite.fr>
2024-02-27 12:51:39 +0100

    Modernized JS syntax

--- resources/ObjectExternal/DemoVueJSFrontend/SCRIPT.js
+++ resources/ObjectExternal/DemoVueJSFrontend/SCRIPT.js
@@ -1,72 +1,34 @@
-var DemoVueJSFrontend = DemoVueJSFrontend || (() => {
-	let app, prd;
-	const data = { item: null, list: null };
+class DemoVueJSFrontend {
 
-	/**
-	 * Render for Vue.js 2.x
-	 * @function
-	 */
-	function render2() {
-		new Vue({
-			el: '#demovuejsfrontend',
-			data: data,
-			beforeMount: () => {
-				data.list = null;
-				data.item = null;
-			},
-			methods: {
-				select: item => data.item = item
-			},
-		});
-
-		prd.search(rows => {
-			data.list = rows;
-		}, null, { inlineDocs: true });
-	}
-	
-	/**
-	 * Render for Vue.js 3.x
-	 * @function
-	 */
-	function render3() {
-		const vue = Vue.createApp({ data: () => data });
-
-		prd.search(rows => {
-			data.list = rows;
-			vue.mount('#demovuejsfrontend');
-		}, null, { inlineDocs: true });
-	}
-
-	/**
-	 * Render
-	 * @param params Parameters
-	 * @function
-	 */
-	function render(params) {
+	static render(params) {
 		try {
 			if (typeof Vue === 'undefined') throw 'Vue.js not available';
 			console.log('Vue.js ' + Vue.version);
 	
-			data.bannerURL = data.bannerURL || params.bannerURL; // Image banner URL
+			const data = {
+				item: null,
+				list: null,
+				bannerURL: params.bannerURL // Image banner URL
+			};
 	
-			app = app || (params.pub ?
-				// External
-				new Simplicite.Ajax(params.root, 'api', 'website', 'simplicite') :
-				// Internal
-				$ui.getAjax()
-			);
+			const app = params.pub ?
+				// External use (public)
+				new Simplicite.Ajax(params.root, 'uipublic') :
+				// Internal UI use
+				$ui.getAjax();
+
+			const prd = app.getBusinessObject('DemoProduct');
 	
-			prd = prd || app.getBusinessObject('DemoProduct');
+			const vue = Vue.createApp({ data: () => data });
 	
-			if (Vue.version.startsWith('2.'))
-				render2();
-			else
-				render3();
+			prd.search(rows => {
+				data.list = rows;
+				vue.mount('#demovuejsfrontend');
+			}, null, { inlineDocs: true });
 		} catch(e) {
 			console.error('Render error: ' + e.message);
 			$('#demovuejsfrontend').text(e.message);
 		}
 	}
 	
-	return { render: render };
-})();
+}
